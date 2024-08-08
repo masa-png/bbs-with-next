5bfb180a5ce92e2a81d4c0075fbe740c9d581e5d
masa-png <adidas.org@icloud.com>
2024-08-08 12:08:38 +0900

    投稿一覧画面、投稿詳細画面の実装

---
+++ app/components/BBSCard.tsx
@@ -0,0 +1,37 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import { BBSData } from "../types/types";
+
+interface BBSDataProps {
+  bbsData: BBSData;
+}
+
+const BBSCard = ({ bbsData }: BBSDataProps) => {
+  const { id, username, title, content, createdAt } = bbsData;
+
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{username}</CardDescription>
+        </CardHeader>
+        <CardContent>{content}</CardContent>
+        <CardFooter className="flex justify-between">
+          <Link href={`/bbs-posts/${id}`} className="text-blue-500">
+            もっと見る
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default BBSCard;
